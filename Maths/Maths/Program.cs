730ce27f701ddd724c5c4c375c7b4fd987e291af
Deep Parmar <deep.parmar@rxteam376.onmicrosoft.com>
2021-08-08 19:07:54 +0530

    comment done

--- Maths/Maths/Program.cs
+++ Maths/Maths/Program.cs
@@ -8,7 +8,7 @@ namespace Maths
         {
             int a = 10, b = 15;
             int c = a + b;
-            Console.WriteLine("This is Addition Program:");
+            Console.WriteLine("This is Addition Program:"); 
             Console.WriteLine("Sum is : {0}", c);
             Console.ReadLine();
         }

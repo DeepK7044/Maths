39dadc87c8756bc2176f926ba0969a87730fcbca
Deep Parmar <deep.parmar@rxteam376.onmicrosoft.com>
2021-08-08 18:56:07 +0530

    add Addition File

---
+++ Maths/Maths/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Maths
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10, b = 15;
+            int c = a + b;
+            Console.WriteLine("This is Addition Program:");
+           
+        }
+    }
+}
